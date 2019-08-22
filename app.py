9349eddbb7b2d21314d51902fe588cbdcf7a9bea
jonburdon <jon@branchoutwebsites.com>
2019-08-22 15:35:33 +0100

    Messages are displayed using br tags

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ def add_messages(username, message):
     """Add messages to the `messages` list"""
     messages.append("{}: {}".format(username, message))
 
+def get_all_messages():
+    """Get all messages and separate using a <br>"""
+    return "<br>".join(messages)
+
 @app.route('/')
 def index():
     """Main page with instructions"""
@@ -17,7 +21,7 @@ def index():
 @app.route('/<username>')
 def user(username):
     """Display chat page"""
-    return "Welcome, {0} - {1}".format(username, messages)
+    return "Welcome, {0} - {1}".format(username, get_all_messages())
 
 @app.route('/<username>/<message>')
 def send_message(username, message):
